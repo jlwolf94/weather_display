8660f9300c48cb39fdfe9db8639a8c450fa21808
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-07-10 20:49:20 +0200

    Add get_station_data and get_display_data methods to DwdData class. Remove redundant methods of the DwdData class.

--- weather_display/dwd_data.py
+++ weather_display/dwd_data.py
@@ -3,6 +3,9 @@ The dwd_data module contains the DwdData class that is used to make all calls
 to the DWD-API and preprocess the retrieved data.
 """
 
+import datetime
+import requests
+
 from deutschland import dwd
 from deutschland.dwd.api import default_api
 from deutschland.dwd.model.station_overview_extended_get_station_ids_parameter_inner \
@@ -28,6 +31,25 @@ class DwdData:
         ----------
         station_identifier (str):
             Station identifier of an existing weather station.
+        """
+
+        self.url = "https://app-prod-ws.warnwetter.de/v30"
+        """
+        url (str):
+            Standard base url for the DWD-API.
+        """
+
+        self.attempts = 3
+        """
+        attempts (int):
+            Number of connection attempts. Default value is 3.
+        """
+
+        self.timeout = 10
+        """
+        timeout (int):
+            Connection timeout for a server answer in seconds.
+            Default value is 10 seconds.
         """
 
         # Defining the host is optional and defaults to https://app-prod-ws.warnwetter.de/v30
@@ -57,6 +79,53 @@ class DwdData:
             Raw data that is retrieved as a json file in the API response and
             converted to a StationOverview object.
         """
+
+    def get_station_data(self, station_info):
+        """
+        Method that retrieves the current weather data for a station specified by
+        its informations from the DWD-API. The recieved data is return in form
+        of a deserialized json file.
+
+        Parameters
+        ----------
+        station_info (dict):
+            A dictionary containing all informations of the station.
+
+        Returns
+        -------
+        station_data (dict):
+            A dictionary containing all current weather data from the
+            station specified by the station_info. An empty dictionary
+            is returned if their is no data.
+        """
+
+        # Add url for a station data get request to the base url.
+        url = self.url + "/stationOverviewExtended"
+
+        # Build the parameters and the headers for the get request.
+        params = {"stationIds": station_info.get("Stations-kennung", "0")}
+        headers = {"accept": "application/json"}
+
+        # Try to reach the server multiple times and handle occuring exceptions.
+        for i in range(self.attempts):
+            try:
+                response = requests.get(url, params=params,
+                                        headers=headers, timeout=self.timeout)
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as err_http:
+                print("HTTP Error:", err_http)
+            except requests.exceptions.ConnectionError as err_con:
+                print("Connection Error:", err_con)
+            except requests.exceptions.Timeout as err_time:
+                print("Timeout Error:", err_time)
+            except requests.exceptions.TooManyRedirects as err_red:
+                print("Redirect Error:", err_red)
+            except requests.exceptions.RequestException as err_req:
+                print("Request Error:", err_req)
+            else:
+                return response.json()
+
+        return {}
 
     def get_data(self):
         """
@@ -115,46 +184,108 @@ class DwdData:
         else:
             pprint(self.raw_data)
 
-    def get_station_name(self):
+    @staticmethod
+    def get_display_data(station_info, station_data):
         """
-        Function that extracts the station name from the raw data.
+        Method that extracts the weather data that will be displayed for
+        the station specified by station_info from the provided station_data.
+        The formatted data used for display is returned in a new dictionary.
+
+        Parameters
+        ----------
+        station_info (dict):
+            A dictionary containing all informations of the station.
+
+        station_data (dict):
+            A dictionary containing all current weather data from the
+            station specified by the station_info.
 
         Returns
         -------
-        station_name (str or None):
-            The station name or None if it is not present.
+        display_data (dict):
+            A dictionary containing all weather data for the given station
+            formatted for display purposes.
         """
 
-        if self.raw_data is None or not self.raw_data:
-            return None
+        # Extract the station identifier and name.
+        stationId = station_info.get("Stations-kennung", "0")
+        station_name = station_info.get("Stationsname", "Error")
+
+        # Check whether forecast data is available.
+        forecast_dict = station_data.get(stationId, {}).get("forecast1", {})
+        if not forecast_dict:
+            # Error values for time and temperature.
+            formatted_time = "--:--"
+            temperature = float("nan")
         else:
-            return self.station_identifier
+            # Extract start date and date step.
+            start_date = datetime.datetime.fromtimestamp(forecast_dict["start"] / 1000)
+            date_step = datetime.timedelta(milliseconds=forecast_dict["timeStep"])
 
-    def get_formatted_time(self):
-        """
-        Function that extracts the closest past time compared to the
-        current time from the raw data and returns it formatted.
+            # Process the temperature list by adding the time to each temperature.
+            date_temp_list = []
+            for step, temp in enumerate(forecast_dict["temperature"]):
+                if temp < -999 or temp > 999:
+                    date_temp_list.append((start_date + (step * date_step), float("nan")))
+                else:
+                    date_temp_list.append((start_date + (step * date_step), temp / 10))
 
-        Returns
-        -------
-        formatted_time (str):
-            Formatted time as HH:MM.
-        """
+            # Find the date closest to the current date.
+            curr_date = datetime.datetime.now()
+            date_temp = min([dt for dt in date_temp_list if dt[0] <= curr_date],
+                            key=lambda t: abs(curr_date - t[0]))
 
-        return "None"
+            formatted_time = date_temp[0].strftime("%H:%M")
+            temperature = date_temp[1]
 
-    def get_current_temperature(self):
-        """
-        Function that extracts and returns the current temperature for
-        the closest time from the raw data.
+        # Check whether days data is available.
+        days_list = station_data.get(stationId, {}).get("days", [])
+        if not days_list:
+            forecast = "Error"
+            daily_min = float("nan")
+            daily_max = float("nan")
+        else:
+            # Find the date in days list closest to the current date.
+            curr_date = datetime.datetime.now()
+            day = min([da for da in days_list
+                       if datetime.datetime.strptime(da["dayDate"], "%Y-%m-%d") <= curr_date],
+                      key=lambda d: abs(curr_date -
+                                        datetime.datetime.strptime(d["dayDate"], "%Y-%m-%d")))
 
-        Returns
-        -------
-        temperature (float):
-            Current temperature from the raw data.
-        """
+            icon_dict = {1: "sun", 2: "sun, slightly cloudy", 3: "sun, cloudy", 4: "clouds",
+                         5: "fog", 6: "fog, risk of slipping", 7: "light rain", 8: "rain",
+                         9: "heavy rain", 10: "light rain, risk of slipping",
+                         11: "heavy rain, risk of slipping", 12: "rain, sporadic snowfall",
+                         13: "rain, increased snowfall", 14: "light snowfall", 15: "snowfall",
+                         16: "heavy snowfall", 17: "clouds, hail", 18: "sun, light rain",
+                         19: "sun, heavy rain", 20: "sun, rain, sporadic snowfall",
+                         21: "sun, rain, increased snowfall", 22: "sun, sporadic snowfall",
+                         23: "sun, increased snowfall", 24: "sun, hail", 25: "sun, heavy hail",
+                         26: "thunderstorm", 27: "thunderstorm, rain",
+                         28: "thunderstorm, heavy rain", 29: "thunderstorm, hail",
+                         30: "thunderstorm, heavy hail", 31: "wind"
+                        }
 
-        return 0.0
+            forecast = icon_dict.get(day["icon"], "Error")
+
+            if day["temperatureMin"] < -999 or day["temperatureMin"] > 999:
+                daily_min = float("nan")
+            else:
+                daily_min = day["temperatureMin"] / 10
+
+            if day["temperatureMax"] < -999 or day["temperatureMax"] > 999:
+                daily_max = float("nan")
+            else:
+                daily_max = day["temperatureMax"] / 10
+
+        # Return all gathered data in a dictionary.
+        return {"station_name": station_name,
+                "formatted_time": formatted_time,
+                "temperature": temperature,
+                "forecast": forecast,
+                "daily_min": daily_min,
+                "daily_max": daily_max
+               }
 
     def test_print(self):
         """
@@ -169,7 +300,7 @@ class DwdData:
         forecast = "sunny"
         print(f'Station: {station_name:30}    '
               f'Time: {formatted_time:19}    '
-              f'Temperature: {temperature:.1f} °C')
+              f'Temperature: {temperature:5.1F} °C')
         print(f'Daily forecast: {forecast:23}    '
-              f'Daily min. temp.: {daily_min:.1f} °C    '
-              f'Daily max. temp.: {daily_max:.1f} °C')
+              f'Daily min. temp.: {daily_min:5.1F} °C    '
+              f'Daily max. temp.: {daily_max:5.1F} °C')
