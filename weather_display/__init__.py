0596684a085ace7bea73bb65c5fe82e92d5154a6
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-07-11 18:17:58 +0200

    Add __init__.py file to the weather_display package.

---
+++ weather_display/__init__.py
@@ -0,0 +1,22 @@
+"""
+# Description of the weather_display package.
+
+The weather_display package contains all modules and classes related to
+the gain, storage and display of weather data from different sources.
+
+# Dependencies and installation
+
+The weather_display package depends on a Python Version >= 3.11 and all
+dependencies that are listed in the `requirements.txt`.
+
+All dependencies can be installed with pip using the provided
+`requirements.txt` and the command `pip install -r requirements.txt`.
+
+# Further remarks
+
+Generating a documentation in HTML format can be achieved by using
+`pdoc` and the following commands, issued in the weather_display parent
+directory: `pdoc ./weather_display -o docs`.
+
+Author: Jan-Lukas Wolf
+"""
