1083d3ce1cabacfb9aac396eaa73af52bd898a85
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-07-12 18:34:34 +0200

    Rework main function for the package in __main__.py.

--- weather_display/__main__.py
+++ weather_display/__main__.py
@@ -7,52 +7,60 @@ from the parent directory.
 import argparse
 import textwrap
 
-from .dwd_stations import DwdStations
-from .dwd_data import DwdData
-from .display_data import DisplayData
-from .display import Display
+from weather_display.dwd_stations import DwdStations
+from weather_display.dwd_data import DwdData
+from weather_display.display import Display
 
 
-# Set up a parser for command line argument parsing.
-parser = argparse.ArgumentParser(
-    prog="weather_display",
-    description=textwrap.dedent("""
-        A simple Python program that retrieves weather data from different sources
-        and displays the data on the console or on a display.
-        """),
-    epilog=textwrap.dedent("""
-        Home page: <https://github.com/jlwolf94/weather_display/>
-        Author: Jan-Lukas Wolf
-        """),
-    formatter_class=argparse.RawDescriptionHelpFormatter)
+def main():
+    """
+    Main function of the weather_display package and command line script.
+    """
 
-# Fill the parser with arguments.
-parser.add_argument("-n", "--name", action="store", default="",
-                    help="name of the weather station")
-parser.add_argument("-x", "--lat", action="store", type=float,
-                    help="geographic coordinate latitude")
-parser.add_argument("-y", "--lon", action="store", type=float,
-                    help="geographic coordinate longitude")
-parser.add_argument("-v", "--version", action="version",
-                    version="%(prog)s 1.0.0")
+    # Set up a parser for command line argument parsing.
+    parser = argparse.ArgumentParser(
+        prog="weather_display",
+        description=textwrap.dedent("""
+            A simple Python program that retrieves weather data from different sources
+            and displays the data on the console or on a display.
+            """),
+        epilog=textwrap.dedent("""
+            Home page: <https://github.com/jlwolf94/weather_display/>
+            Author: Jan-Lukas Wolf
+            """),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
 
-# Process the command line arguments.
-args = parser.parse_args()
+    # Fill the parser with arguments.
+    parser.add_argument("-n", "--name", action="store", default="",
+                        help="name of the weather station")
+    parser.add_argument("-x", "--lat", action="store", type=float,
+                        help="geographic coordinate latitude")
+    parser.add_argument("-y", "--lon", action="store", type=float,
+                        help="geographic coordinate longitude")
+    parser.add_argument("-v", "--version", action="version",
+                        version="%(prog)s 1.0.0")
 
-# Get the stations table.
-dwd_stations = DwdStations()
-dwd_stations.update()
+    # Process the command line arguments.
+    args = parser.parse_args()
 
-# If latitude and longitude is available use the geographic coordinates.
-if args.lat is not None and args.lon is not None:
-    station_info = dwd_stations.get_station_info_by_distance(args.lat, args.lon)
-else:
-    station_info = dwd_stations.get_station_info_by_name(args.name)
+    # Get the stations table.
+    dwd_stations = DwdStations()
+    dwd_stations.update()
 
-# Get the station data.
-dwd_data = DwdData(station_info)
-dwd_data.update()
+    # If latitude and longitude is available use the geographic coordinates.
+    if args.lat is not None and args.lon is not None:
+        station_info = dwd_stations.get_station_info_by_distance(args.lat, args.lon)
+    else:
+        station_info = dwd_stations.get_station_info_by_name(args.name)
 
-# Show the station data on the console.
-display = Display()
-display.show(dwd_data.get_display_data())
+    # Get the station data.
+    dwd_data = DwdData(station_info)
+    dwd_data.update()
+
+    # Show the station data on the console.
+    display = Display()
+    display.show(dwd_data.get_display_data())
+
+
+if __name__ == "__main__":
+    main()
