0a35f47e99d6e6a75b39c2a6f8ca2e24d66e4fd5
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-08-19 21:15:54 +0200

    Add new data source to the main method of the package in the start module.

--- weather_display/start.py
+++ weather_display/start.py
@@ -7,8 +7,11 @@ import sys
 import argparse
 import textwrap
 
+from weather_display.models.display_data import DisplayData
+from weather_display.models.station import Station
+from weather_display.collectors.dwd_data import DwdData
+from weather_display.collectors.w24_data import W24Data
 from weather_display.dwd_stations import DwdStations
-from weather_display.dwd_data import DwdData
 from weather_display.display import Display
 
 
@@ -35,10 +38,14 @@ def main():
     # Fill the parser with arguments.
     parser.add_argument("-n", "--name", action="store", default="Error",
                         help="name of the weather station")
+    parser.add_argument("-i", "--id", action="store", type=int,
+                        help="identifier of the weather station")
     parser.add_argument("-x", "--lat", action="store", type=float,
                         help="geographic coordinate latitude")
     parser.add_argument("-y", "--lon", action="store", type=float,
                         help="geographic coordinate longitude")
+    parser.add_argument("-s", "--src", action="store", default=0,
+                        type=int, help="number of the data source")
     parser.add_argument("-v", "--version", action="version",
                         version="%(prog)s 1.1.0")
 
@@ -50,23 +57,41 @@ def main():
         parser.print_help()
         return
 
-    # Get the stations table.
-    dwd_stations = DwdStations()
-    dwd_stations.update()
+    # Check which data source is selected.
+    if args.src >= 1:
+        # Initialize the display.
+        display = Display()
 
-    # If latitude and longitude is available use the geographic coordinates.
-    if args.lat is not None and args.lon is not None:
-        station = dwd_stations.get_station_by_distance(args.lat, args.lon)
+        # Check whether a name and an identifier is present.
+        if args.name == "Error" or args.id is None:
+            # Show empty Data.
+            display.show(DisplayData())
+        else:
+            # Get the station data.
+            station = Station(name=args.name, number=args.id)
+            w24_data = W24Data(station)
+            w24_data.update()
+
+            # Show the retrieved data.
+            display.show(w24_data.get_display_data())
     else:
-        station = dwd_stations.get_station_by_name(args.name)
+        # Get the stations table.
+        dwd_stations = DwdStations()
+        dwd_stations.update()
 
-    # Get the station data.
-    dwd_data = DwdData(station)
-    dwd_data.update()
+        # If latitude and longitude is available use the geographic coordinates.
+        if args.lat is not None and args.lon is not None:
+            station = dwd_stations.get_station_by_distance(args.lat, args.lon)
+        else:
+            station = dwd_stations.get_station_by_name(args.name)
 
-    # Show the station data on the console.
-    display = Display()
-    display.show(dwd_data.get_display_data())
+        # Get the station data.
+        dwd_data = DwdData(station)
+        dwd_data.update()
+
+        # Show the station data on the console.
+        display = Display()
+        display.show(dwd_data.get_display_data())
 
 
 if __name__ == "__main__":
