89281620cf45715e50322bec2470f6e63841d693
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-08-19 19:16:54 +0200

    Add collectors sub-package and move the weather data collecting modules to this package.

---
+++ weather_display/collectors/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Description of the collectors sub-package
+
+The collectors package is a sub-package of the weather_display package and
+contains all modules and classes that are responsible for the collection
+of weather data from different sources. The collected weather data is
+used by the modules and classes of the weather_display package.
+
+Author: Jan-Lukas Wolf
+"""
