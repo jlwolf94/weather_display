4802e5634f34d918e02fdacd61c4f0399d8642ec
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-08-22 18:10:52 +0200

    Add init file for the new displays sub-package.

---
+++ weather_display/displays/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Description of the displays sub-package
+
+The displays package is a sub-package of the weather_display package and
+contains all modules and classes that are responsible for the configuration
+and control of the different displays. The different displays are used
+to show the gathered weather data. Weather data can be gathered with the
+modules and classes of the collectors sub-package.
+
+Author: Jan-Lukas Wolf
+"""
