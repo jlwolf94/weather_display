553a763885425fa0b59863bf3723c9ac1de429fa
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-08-28 21:04:10 +0200

    Refactor lcd_144_test module.

--- weather_display/displays/lcd_144_test.py
+++ weather_display/displays/lcd_144_test.py
@@ -3,10 +3,8 @@ The lcd_144_test module contains a main function that is used to test the
 functionalities of the 1.44inch LCD HAT SPI interface from Waveshare.
 """
 
-import RPi.GPIO as GPIO
-
 from pathlib import Path
-from PIL import Image, ImageDraw, ImageFont, ImageColor
+from PIL import Image, ImageDraw
 from weather_display.displays.lcd_144 import LCD144
 
 
@@ -21,45 +19,49 @@ def main():
     """
 
     try:
-        # Create LCD object for configuration and control.
-        LCD = LCD144()
+        # Create LCD display object for configuration and control.
+        display = LCD144()
 
-        print("****init LCD****")
-        # SCAN_DIR_DFT should be equal D2U_L2R
-        scan_dir = 6
-        LCD.init_LCD(scan_dir)
-        LCD.clear()
+        print("init LCD display")
+        if display.init_LCD() == 1:
+            return 1
+        display.clear()
 
-        image = Image.new("RGB", (LCD.width, LCD.height), "WHITE")
+        # Create image to draw on.
+        image = Image.new("RGB", (display.width, display.height), "WHITE")
         draw = ImageDraw.Draw(image)
 
-        # font = ImageFont.truetype(
-        #     "/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 16)
-
-        print("****draw line****")
+        print("draw line")
         draw.line([(0, 0), (127, 0)], fill="BLUE", width=5)
         draw.line([(127, 0), (127, 127)], fill="BLUE", width=5)
         draw.line([(127, 127), (0, 127)], fill="BLUE", width=5)
         draw.line([(0, 127), (0, 0)], fill="BLUE", width=5)
 
-        print("****draw rectangle****")
+        print("draw rectangle")
         draw.rectangle([(18, 10), (110, 20)], fill="RED")
 
-        print("****draw text****")
+        print("draw text")
+        # font = ImageFont.truetype(
+        #     "/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf", 16)
         draw.text((33, 22), "WaveShare ", fill="BLUE")
         draw.text((32, 36), "Electronic ", fill="BLUE")
         draw.text((28, 48), "1.44inch LCD ", fill="BLUE")
 
-        # image.save(Path(__file__).parents[0].joinpath("test.png"))
+        # Save image and show it on the display.
+        image.save(Path(__file__).parents[0].joinpath("test.png"))
+        display.show_image(image)
+        display.config.delay_driver_ms(500)
 
-        LCD.show_image(image)
-        LCD.config.delay_driver_ms(500)
-
+        # Open image and show it on the display.
         image = Image.open(Path(__file__).parents[0].joinpath("sky.bmp"))
-        LCD.show_image(image)
+        display.show_image(image)
+        display.config.delay_driver_ms(500)
 
+        print("test successful")
         return 0
     except:
+        print("error during test")
         return 1
     finally:
-        GPIO.cleanup()
+        print("cleanup executed")
+        LCD144.cleanup_GPIO()
