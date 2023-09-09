3d093e0d98a45e026e2d3fcaf550378132f1289c
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-09-09 18:46:09 +0200

    Add create_data_image_128_128 method to Display class and set a new dark mode property for the class.

--- weather_display/display.py
+++ weather_display/display.py
@@ -4,6 +4,8 @@ weather data handed to the class or its methods on the selected output channel.
 The weather data is formatted to align with the chosen output channel.
 """
 
+from PIL import Image, ImageFont, ImageDraw
+
 
 class Display:
     """
@@ -27,7 +29,7 @@ class Display:
         color (c) color mode.
     """
 
-    def __init__(self, output=0, width=128, height=128, color_mode="bw"):
+    def __init__(self, output=0, width=128, height=128, dark_mode=False):
         """
         Constructor for the Display objects.
 
@@ -45,10 +47,9 @@ class Display:
             Height of the output display in pixels.
             The default value is 128 pixels.
 
-        color_mode (str):
-            A string that is part of the COLOR_MODES tuple and
-            represent the black and white or color color mode.
-            The default value is the black and white color mode (bw).
+        dark_mode (bool):
+            A boolean that indicates whether the dark mode is
+            active or not. The default value is False.
         """
 
         self.output = output if output in self.OUTPUTS else 0
@@ -72,13 +73,78 @@ class Display:
             The default value is 128 pixels.
         """
 
-        self.color_mode = color_mode if color_mode in self.COLOR_MODES else "bw"
+        self.dark_mode = dark_mode
         """
-        color_mode (str):
-            A string that is part of the COLOR_MODES tuple and
-            represent the black and white or color color mode.
-            The default value is the black and white color mode (bw).
+        dark_mode (bool):
+            A boolean that indicates whether the dark mode is
+            active or not.
         """
+
+    def create_data_image_128_128(self, display_data):
+        """
+        Method that creates an image of 128 x 128 pixel that contains the data
+        of the given DisplayData object.
+
+        Parameters
+        ----------
+        display_data (DisplayData):
+            A DisplayData object containing all weather data to be shown on
+            the image.
+
+        Returns
+        -------
+        image (Image):
+            Created image that contains all data of the DisplayData object.
+        """
+
+        # Prepare image and color settings.
+        image_mode = "RGB"
+        image_size = (128, 128)
+        font_size = 10
+        if self.dark_mode:
+            back_color = "BLACK"
+            text_color = "WHITE"
+        else:
+            back_color = "WHITE"
+            text_color = "BLACK"
+
+        # Load the selected font or fall back to default font.
+        try:
+            font = ImageFont.truetype(
+                font="/usr/share/fonts/truetype/noto/NotoSansMono-Regular.ttf",
+                size=font_size)
+        except OSError as err_os:
+            print("Font Error:", err_os)
+            font = None
+
+        image = Image.new(mode=image_mode, size=image_size, color=back_color)
+        draw = ImageDraw.Draw(image)
+
+        # Truncate to long station names, forecasts and dates.
+        station_name = (display_data.station_name[:16] + ".") \
+            if len(display_data.station_name) > 17 else display_data.station_name
+        formatted_forecast = display_data.get_formatted_forecast()
+        forecast = (formatted_forecast[:9] + ".") \
+            if len(formatted_forecast) > 10 else formatted_forecast
+        date = display_data.get_formatted_date().split(" ")[1]
+
+        # Draw image data.
+        draw.text((10, 7), station_name, fill=text_color, font=font)
+        draw.text((10, 21), f"{date}, {display_data.get_formatted_time()}",
+                  fill=text_color, font=font)
+        draw.text((10, 35), f"Fore.: {forecast}", fill=text_color, font=font)
+        draw.text((10, 49), f"Tmax: {display_data.daily_max:5.1F} °C",
+                  fill=text_color, font=font)
+        draw.text((10, 63), f"Tmin: {display_data.daily_min:5.1F} °C",
+                  fill=text_color, font=font)
+        draw.text((10, 77), f"T:  {display_data.temperature:5.1F} °C",
+                  fill=text_color, font=font)
+        draw.text((10, 91), f"Td: {display_data.dew_point:5.1F} °C",
+                  fill=text_color, font=font)
+        draw.text((10, 105), f"Prec.: {display_data.precipitation:4.1F} mm",
+                  fill=text_color, font=font)
+
+        return image
 
     def output_to_console(self, display_data):
         """
