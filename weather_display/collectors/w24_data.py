61ad1b11736fb28b0b9ff661952710d227ee4c06
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-09-11 15:10:43 +0200

    Refactor w24_data module to use the Data abstract base class.

--- weather_display/collectors/w24_data.py
+++ weather_display/collectors/w24_data.py
@@ -1,17 +1,17 @@
 """
-The w24_data module contains the W24Data class that is used to download and parse
+The data_w24 module contains the DataW24 class that is used to download and parse
 the wetter24 website, save the data and preprocess the extracted data for display purposes.
 """
 
 import json
-import requests
 
 from datetime import datetime
 from bs4 import BeautifulSoup
+from weather_display.collectors.data import Data
 from weather_display.models.display_data import DisplayData
 
 
-class W24Data:
+class DataW24(Data):
     """
     Class that contains the wetter24 base url and stores the station informations
     used for the website requests. It stores the received data for later use or display.
@@ -19,7 +19,7 @@ class W24Data:
 
     def __init__(self, station, attempts=3, timeout=10):
         """
-        Constructor for the W24Data objects.
+        Constructor for the Data objects.
 
         Parameters
         ----------
@@ -34,56 +34,40 @@ class W24Data:
             Default value is 10 seconds.
         """
 
-        self.station = station
-        """
-        station (Station):
-            Station object containing all informations of the station.
-        """
+        super().__init__(station, attempts, timeout)
 
-        self.url = "http://www.wetter24.de/wetterstation"
+        self.url = "http://www.wetter24.de/wetterstation" + "/" \
+            + station.name.lower() + "/" + str(station.number)
         """
         url (str):
-            Standard base url for the wetter24 weather station site.
+            Standard url for the get requests.
+        """
+
+        self.params = None
+        """
+        params (Optional[dict[str, str]]):
+            Dictionary with all parameters for the get request.
         """
 
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) " \
             + "Gecko/20100101 Firefox/114.0"
         self.headers = {"User-Agent": user_agent}
         """
-        headers (dict[str, str]):
-            Dictionary with all header parameters for the request.
+        headers (Optional[dict[str, str]]):
+            Dictionary with all header parameters for the get request.
         """
 
-        self.attempts = attempts
-        """
-        attempts (int):
-            Number of connection attempts.
-        """
-
-        self.timeout = timeout
-        """
-        timeout (int):
-            Connection timeout for a server answer in seconds.
-        """
-
-        self.station_data = {}
-        """
-        station_data (dict):
-            A dictionary containing all current weather data from the
-            station specified by station.
-        """
-
-    @staticmethod
-    def process_stations_page(response):
+    def get_station_data(self, response):
         """
         Method that processes the response of a request to the
-        standard url and returns a formatted dictionary of the data.
+        standard url with set parameters and headers. It returns
+        a formatted dictionary of the data.
 
         Parameters
         ----------
         response (Optional[Response]):
             A response object retrieved from a request to the standard url
-            or None.
+            with the set parameters and headers or None.
 
         Returns
         -------
@@ -110,41 +94,6 @@ class W24Data:
         data_string = scripts[0].get_text().split("initWeatherStation(")[1].split(")")[0]
         return json.loads(data_string)
 
-    def get_station_data(self):
-        """
-        Method that triggers a get request for the standard url and
-        handles all possible error cases.
-
-        Returns
-        -------
-        response (Optional[Response]):
-            The response object of the request to the standard url or None.
-        """
-
-        # Add station name and number to the base url for the get request.
-        url = self.url + "/" + self.station.name.lower() + "/" + str(self.station.number)
-
-        # Try to reach the server multiple times and handle occuring exceptions.
-        for i in range(self.attempts):
-            try:
-                response = requests.get(url, headers=self.headers,
-                                        timeout=self.timeout)
-                response.raise_for_status()
-            except requests.exceptions.HTTPError as err_http:
-                print("HTTP Error:", err_http)
-            except requests.exceptions.ConnectionError as err_con:
-                print("Connection Error:", err_con)
-            except requests.exceptions.Timeout as err_time:
-                print("Timeout Error:", err_time)
-            except requests.exceptions.TooManyRedirects as err_red:
-                print("Redirect Error:", err_red)
-            except requests.exceptions.RequestException as err_req:
-                print("Request Error:", err_req)
-            else:
-                return response
-
-        return None
-
     def get_display_data(self):
         """
         Method that extracts the weather data that will be displayed from
@@ -154,7 +103,7 @@ class W24Data:
         Returns
         -------
         display_data (DisplayData):
-            A DisplayData object containing all weather data for the given
+            A DisplayData object containing all weather data for the set
             station formatted for display purposes.
         """
 
@@ -212,24 +161,3 @@ class W24Data:
 
         # Return all gathered data in a DisplayData object.
         return display_data
-
-    def update(self):
-        """
-        Method that updates or retrieves the station_data from the wetter24 website
-        with the informations saved in station.
-
-        Returns
-        -------
-        success (bool):
-            Indicates whether the update process was a success or not.
-        """
-
-        # Try to get station data from the wetter24 website.
-        station_data = self.process_stations_page(self.get_station_data())
-
-        # Check whether there is data for an update.
-        if station_data:
-            self.station_data = station_data
-            return True
-        else:
-            return False
