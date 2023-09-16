9a7d1428de674e7e2bb5053d6ac27d6c06f80271
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-09-16 17:39:31 +0200

    Add combine_display_data and get_display_data method to CollectorsController class.

--- weather_display/collectors/collectors_controller.py
+++ weather_display/collectors/collectors_controller.py
@@ -5,6 +5,7 @@ gather and update data contained in the different data sources.
 """
 
 from weather_display.models.station import Station
+from weather_display.models.display_data import DisplayData
 from weather_display.collectors.data_dwd import DataDWD
 from weather_display.collectors.data_w24 import DataW24
 from weather_display.collectors.data_won import DataWon
@@ -50,6 +51,7 @@ class CollectorsController:
             Default value is False.
         """
 
+        # Initialize the data sources.
         if stations is None or not stations:
             # Set a default station for the default data source.
             self.data_sources.update({self.SOURCES[0]: DataDWD(Station())})
@@ -62,6 +64,81 @@ class CollectorsController:
                     self.data_sources.update({self.SOURCES[2]: DataWon(station)})
                 else:
                     self.data_sources.update({self.SOURCES[0]: DataDWD(station)})
+
+    @staticmethod
+    def combine_display_data(dd_res, dd_new):
+        """
+        Method that updates the result DisplayData object with new data of
+        the new DisplayData object if it does not match the default data
+        in a default DisplayData object.
+
+        Parameters
+        ----------
+        dd_res (DisplayData):
+            DisplayData object with the current result data.
+
+        dd_new (DisplayData):
+            DisplayData object with the new data.
+
+        Returns
+        -------
+        dd_res (DisplayData):
+            The updated result DisplayData object.
+        """
+
+        # Get default DisplayData object for comparison.
+        dd_def = DisplayData()
+
+        if (dd_new.station_name != dd_def.station_name
+            and dd_new.station_name != dd_res.station_name):
+            dd_res.station_name = dd_new.station_name
+
+        if (dd_new.date_time is not None
+            and dd_new.date_time >= dd_res.date_time):
+            dd_res.date_time = dd_new.date_time
+            dd_res.temperature = dd_new.temperature
+            dd_res.dew_point = dd_new.dew_point
+            if (dd_new.precipitation != dd_def.precipitation):
+                dd_res.precipitation = dd_new.precipitation
+
+        if (dd_new.forecast != dd_def.forecast
+            and dd_new.forecast != dd_res.forecast):
+            dd_res.forecast = dd_new.forecast
+            dd_res.daily_min = dd_new.daily_min
+            dd_res.daily_max = dd_new.daily_max
+
+        return dd_res
+
+    def get_display_data(self):
+        """
+        Method that gets the weather data that will be displayed from
+        the different data sources. The combined data of all data sources
+        is returned in a new DisplayData object.
+
+        Returns
+        -------
+        display_data (DisplayData):
+            A DisplayData object containing all weather data from all
+            data sources formatted for display purposes.
+        """
+
+        # Check whether an update of the data is necessary.
+        if not self.is_updated:
+            self.update()
+
+        # Combine the weather data in a result DisplayData object.
+        dd_res = DisplayData()
+        is_first = True
+        for data in self.data_sources.values():
+            if is_first:
+                dd_res = data.get_display_data()
+                is_first = False
+            else:
+                dd_res = self.combine_display_data(dd_res, data.get_display_data())
+
+        # Reset the update status and return the result data.
+        self.is_updated = False
+        return dd_res
 
     def update(self):
         """
