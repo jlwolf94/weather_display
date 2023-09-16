bc6b62335bb3c58a45744358133e12051eac7879
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-09-16 13:29:15 +0200

    Add default value for station name to station model.

--- weather_display/models/station.py
+++ weather_display/models/station.py
@@ -9,7 +9,7 @@ class Station:
     Class that contains all informations of a weather station.
     """
 
-    def __init__(self, name, number=0, type="", identifier="0", latitude=0.0,
+    def __init__(self, name="Error", number=0, type="", identifier="0", latitude=0.0,
                  longitude=0.0, altitude=0, river_basin="", state="",
                  start=None, end=None):
         """
@@ -18,7 +18,7 @@ class Station:
         Parameters
         ----------
         name (str):
-            Name of the station.
+            Name of the station. Default name is Error.
 
         number (int):
             Number of the station. Default value is 0.
