c0d9fa9774c09adcd09e6c2f6165a8bc79923957
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2023-09-17 11:55:34 +0200

    Add load_config_from_json method to utils module.

--- weather_display/utils.py
+++ weather_display/utils.py
@@ -2,6 +2,8 @@
 The utils module contains utility functions for the weather_display package.
 """
 
+import json
+
 from pathlib import Path
 
 
@@ -27,3 +29,35 @@ def is_raspberry_pi():
                 return False
     except OSError:
         return False
+
+def load_config_from_json(path):
+    """
+    Method that loads a configuration from a json file that is located at
+    the given file path. The file needs to be a compatible json file.
+
+    Parameters
+    ----------
+    path (str):
+        The absolute path to the json file as a string.
+
+    Returns
+    -------
+    config (dict[str, dict[str, str]]):
+        A dictionary containing all configuration settings or an empty
+        dictionary if no data could be loaded.
+    """
+
+    # Convert string to Path and check whether the file extension is correct.
+    file_extension = ".json"
+    if file_extension in path.lower():
+        file_path = Path(path)
+    else:
+        file_path = Path(path + file_extension)
+
+    # Try to load the configuration from the file.
+    try:
+        with file_path.open(encoding="utf-8") as file:
+            return json.load(file)
+    except OSError as err_os:
+        print("I/O Error:", err_os)
+        return {}
