b8c8583aeb066b817b34b90a8891ac646d892c96
Jan-Lukas Wolf <jan-lukas-wolf@gmx.de>
2024-01-13 16:04:36 +0100

    Refactor start module.

--- weather_display/start.py
+++ weather_display/start.py
@@ -3,159 +3,181 @@ The start module contains the main function of the weather_display package
 and can be used as an entry point script for the package.
 """
 
-import sys
 import argparse
+import sys
 import textwrap
 
-from weather_display.models.station import Station
-from weather_display.collectors.stations_dwd import StationsDWD
 from weather_display.collectors.collector import Collector
-from weather_display.displays.display import Display
+from weather_display.collectors.stations_dwd import StationsDWD
 from weather_display.controller import Controller
+from weather_display.displays.display import Display
+from weather_display.models.station import Station
 from weather_display.utils import get_data_directory, load_config_from_json
 
-
-def get_argument_parser():
-    """
-    Method that builds and returns an argument parser that is setup
-    to act as a CLI for the weather_display package.
-
-    Returns
-    -------
-    parser (ArgumentParser):
-        The configurated argument parser for the CLI.
-    """
-
-    # Set up a parser for command line argument parsing.
-    parser = argparse.ArgumentParser(
-        prog="weather_display",
-        description=textwrap.dedent("""
-            A simple Python program that retrieves weather data from different sources
-            and displays the data on the console or on a display.
-            """
-        ),
-        epilog=textwrap.dedent("""
-            Home page: <https://github.com/jlwolf94/weather_display/>
-            Author: Jan-Lukas Wolf
-            """
-        ),
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    # Fill the parser with arguments.
-    parser.add_argument("-n", "--name", action="store", default="Error",
-                        help="name of the weather station")
-    parser.add_argument("-i", "--id", action="store",
-                        help="identifier of the weather station")
-    parser.add_argument("-x", "--lat", action="store", type=float,
-                        help="geographic coordinate latitude")
-    parser.add_argument("-y", "--lon", action="store", type=float,
-                        help="geographic coordinate longitude")
-    parser.add_argument("-s", "--src", action="store", default=0,
-                        type=int, help="number of the data source")
-    parser.add_argument("-d", "--dir", action="store",
-                        help="path to config and data directory")
-    parser.add_argument("-o", "--out", action="store", default=0,
-                        type=int, help="number of the output channel")
-    parser.add_argument("-m", "--mode", action="store_true",
-                        help="dark mode setting for the output")
-    parser.add_argument("-v", "--version", action="version",
-                        version="%(prog)s 1.4.0")
-
-    return parser
-
-def get_station_from_args(source, args, data_directory=None):
-    """
-    Method that creates a Station object with the given data source,
-    options specified in the arguments and data directory.
-
-    Parameters
-    ----------
-    source (str):
-        Name or number of the selected data source.
-
-    args (Namespace):
-        A namespace created by the argument parser with all arguments.
-
-    data_directory (Optional[Path]):
-        The path to the config and data directory or None.
-
-    Returns
-    -------
-    station (Station):
-        Created station with the set options.
-    """
-
-    # Create station with arguments for the selected data source.
-    if source == Collector.SOURCES[1] or source == "1":
-        try:
-            converted_id = int(args.id)
-        except TypeError:
-            converted_id = 0
-        except ValueError:
-            converted_id = 0
-
-        station = Station(name=args.name, number=converted_id)
-    elif source == Collector.SOURCES[2] or source == "2":
-        if args.id is not None:
-            station = Station(name=args.name, identifier=args.id)
-        else:
-            station = Station(name=args.name)
-    else:
-        stations_dwd = StationsDWD(data_directory=data_directory)
-        stations_dwd.update()
-
-        if args.lat is not None and args.lon is not None:
-            station = stations_dwd.get_station_by_distance(args.lat, args.lon)
-        else:
-            station = stations_dwd.get_station_by_name(args.name)
-
-    return station
 
 def main():
     """
     Main function of the weather_display package and command line script.
 
-    Returns
-    -------
-    success (int):
-        A successful run returns zero and all other runs return one.
+    Returns:
+        int: A successful run returns zero and all other runs return one.
     """
-
-    # Get the argument parser and process the command line arguments.
-    parser = get_argument_parser()
+    parser = _create_argument_parser()
     args = parser.parse_args()
 
-    # Check for arguments and print the help if no arguments are present.
     if len(sys.argv) == 1:
         parser.print_help()
-        return 0
+        return 1
 
-    # Configure the collector with a file configuration or the given arguments.
+    collector = _create_collector(parser, args)
+    display = _create_display(args)
+
+    _start_display(display, collector)
+
+    return 0
+
+
+def _create_argument_parser():
+    parser = _create_argument_parser_with_default_settings()
+    _add_default_arguments_to_argument_parser(parser)
+    return parser
+
+
+def _create_argument_parser_with_default_settings():
+    return argparse.ArgumentParser(
+        prog="weather_display",
+        description=textwrap.dedent(
+            """
+            A simple Python program that retrieves weather data from different sources
+            and displays the data on the console or on a display.
+            """
+        ),
+        epilog=textwrap.dedent(
+            """
+            Home page: <https://github.com/jlwolf94/weather_display/>
+            Author: Jan-Lukas Wolf
+            """
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+
+def _add_default_arguments_to_argument_parser(parser):
+    parser.add_argument(
+        "-n", "--name", action="store", default="Error", help="name of the weather station"
+    )
+    parser.add_argument("-i", "--id", action="store", help="identifier of the weather station")
+    parser.add_argument(
+        "-x", "--lat", action="store", type=float, help="geographic coordinate latitude"
+    )
+    parser.add_argument(
+        "-y", "--lon", action="store", type=float, help="geographic coordinate longitude"
+    )
+    parser.add_argument(
+        "-s", "--src", action="store", default=0, type=int, help="number of the data source"
+    )
+    parser.add_argument("-d", "--dir", action="store", help="path to config and data directory")
+    parser.add_argument(
+        "-o", "--out", action="store", default=0, type=int, help="number of the output channel"
+    )
+    parser.add_argument(
+        "-m", "--mode", action="store_true", help="dark mode setting for the output"
+    )
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.4.0")
+
+
+def _create_collector(parser, args):
     if args.dir is not None:
-        data_directory = get_data_directory(args.dir)
-        config = load_config_from_json(data_directory)
-        stations = {}
-        for source, options in config.items():
-            station_args = parser.parse_args(options)
-            station = get_station_from_args(str(source),
-                                            station_args,
-                                            data_directory)
-            stations.update({str(source): station})
-
-        collector = Collector(stations)
+        collector = _create_collector_from_file_configuration(parser, args)
     else:
-        station = get_station_from_args(str(args.src), args)
-        collector = Collector({str(args.src): station})
+        collector = _create_collector_from_arguments(args)
 
-    # Configure the display and start the controller if necessary.
-    display = Display(output=args.out, dark_mode=args.mode)
+    return collector
+
+
+def _create_collector_from_file_configuration(parser, args):
+    data_directory = get_data_directory(args.dir)
+    config = load_config_from_json(data_directory)
+    stations = {}
+
+    for source, options in config.items():
+        station_args = parser.parse_args(options)
+        station = create_station_from_arguments(str(source), station_args, data_directory)
+        stations.update({str(source): station})
+
+    return Collector(stations)
+
+
+def _create_collector_from_arguments(args):
+    station = create_station_from_arguments(str(args.src), args)
+    return Collector({str(args.src): station})
+
+
+def create_station_from_arguments(source, args, data_directory=None):
+    """
+    Method that creates a Station object with the given data source, options specified in
+    the arguments and data directory.
+
+    Args:
+        source (str): Name or number of the selected data source.
+        args (Namespace): A Namespace created by the ArgumentParser with all arguments.
+        data_directory (Path, optional): The path to the config and data directory or None.
+
+    Returns:
+        Station: Created Station with the set options.
+    """
+    if source == Collector.SOURCES[1] or source == "1":
+        station = _create_w24_station(args)
+    elif source == Collector.SOURCES[2] or source == "2":
+        station = _create_won_station(args)
+    else:
+        station = _create_dwd_station(args, data_directory)
+
+    return station
+
+
+def _create_w24_station(args):
+    try:
+        converted_id = int(args.id)
+    except TypeError:
+        converted_id = 0
+    except ValueError:
+        converted_id = 0
+
+    return Station(name=args.name, number=converted_id)
+
+
+def _create_won_station(args):
+    if args.id is not None:
+        station = Station(name=args.name, identifier=args.id)
+    else:
+        station = Station(name=args.name)
+
+    return station
+
+
+def _create_dwd_station(args, data_directory):
+    stations_dwd = StationsDWD(data_directory=data_directory)
+    stations_dwd.update()
+
+    if args.lat is not None and args.lon is not None:
+        station = stations_dwd.get_station_by_distance(args.lat, args.lon)
+    else:
+        station = stations_dwd.get_station_by_name(args.name)
+
+    return station
+
+
+def _create_display(args):
+    return Display(output=args.out, dark_mode=args.mode)
+
+
+def _start_display(display, collector):
     if display.output == Display.OUTPUTS[1]:
         controller = Controller(collector, display)
         controller.run()
     else:
         display.show(collector.get_display_data())
-
-    return 0
 
 
 if __name__ == "__main__":
